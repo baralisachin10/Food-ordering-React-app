422801243e8ba6893927486556e49d4992f718c7
sachin barali <sachinbarali41@gmail.com>
2023-10-05 02:48:15 +0545

    cart logic management

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Header from "./components/Layouts/Header";
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
+  const [cartIsShown, setcartIsShown] = useState(false)
+
+  const showCartHandler = () => {
+    setcartIsShown(true)
+  }
+
+  const hideCartHandler = () => {
+    setcartIsShown(false)
+  }
   return (
-    <React.Fragment>
-      <Cart/>
-      <Header />
+    <CartProvider>
+      {cartIsShown && <Cart onCloseCart = {hideCartHandler}/>}
+      <Header onShowCart = {showCartHandler} />
       <main>
         <Meals/>
       </main>
-    </React.Fragment>
+    </CartProvider>
   );
 }
 
