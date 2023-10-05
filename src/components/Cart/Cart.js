fbe3b2799f64f1d317f98e48fccadb0a588d82fb
sachin barali <sachinbarali41@gmail.com>
2023-10-05 15:13:01 +0545

    cart logic finished

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,26 +1,54 @@
-import React from 'react'
-import classes from './Cart.module.css'
-import Modal from '../UI/Modal'
+import React, { useContext } from "react";
+import classes from "./Cart.module.css";
+import Modal from "../UI/Modal";
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
-    const cartItems = <ul className={classes['cart-items']}>
-        {[{ id: 'c1', name: 'sushi', amount: 2, price: 12.99 },].map(item => {
-            return <li>{item.name}</li>
-        })}
-    </ul>
-    return (
-        <Modal onClose = {props.onCloseCart}>
-            {cartItems}
-            <div className={classes.total}>
-                <span>Total Amount</span>
-                <span>33.55</span>
-            </div>
-            <div className={classes.actions}>
-                <button onClick={props.onCloseCart} className={classes['button--alt']}>Close</button>
-                <button className={classes.button}>Order</button>
-            </div>
-        </Modal>
-    )
-}
+  const cartCtx = useContext(CartContext);
 
-export default Cart
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItem = cartCtx.items.length > 0;
+
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
+
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {cartCtx.items.map((item) => {
+        return (
+          <CartItem
+            key={item.id}
+            name={item.name}
+            price={item.price}
+            amount={item.amount}
+            onAdd={cartItemAddHandler.bind(null, item)}
+            onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          />
+        );
+      })}
+    </ul>
+  );
+  return (
+    <Modal onClose={props.onCloseCart}>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>{totalAmount}</span>
+      </div>
+      <div className={classes.actions}>
+        <button onClick={props.onCloseCart} className={classes["button--alt"]}>
+          Close
+        </button>
+        {hasItem && <button className={classes.button}>Order</button>}
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
--- src/components/Layouts/HeaderCartButton.js
+++ src/components/Layouts/HeaderCartButton.js
@@ -1,26 +1,42 @@
-import React, { useContext } from 'react'
-import CartIcon from '../Cart/CartIcon'
-import classes from './HeaderCartButton.module.css'
-import CartContext from '../../store/cart-context'
+import React, { useContext, useEffect, useState } from "react";
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
-    const cartCtx = useContext(CartContext);
+  const cartCtx = useContext(CartContext);
+  const [isBtnHighlited, setIsBtnHighlited] = useState(false);
 
-    const numberOfItems = cartCtx.items.reduce((curNumber, item)=>{
-        return curNumber + item.amount;
-    },0)
+  const { items } = cartCtx;
+  const numberOfItems = items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
 
-    return (
-        <button className={classes.button} onClick={props.onClick}>
-            <span className={classes.icon}>
-                <CartIcon />
-            </span>
-            <span>Your Cart</span>
-            <span className={classes.badge}>
-                {numberOfItems}
-            </span>
-        </button>
-    )
-}
+  const btnClasses = `${classes.button} ${isBtnHighlited ? classes.bump : ""}`;
 
-export default HeaderCartButton
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setIsBtnHighlited(true);
+    const timer = setTimeout(() => {
+      setIsBtnHighlited(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
+  return (
+    <button className={btnClasses} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfItems}</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
